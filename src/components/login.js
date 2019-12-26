aecad031642e8684dd2d2d055ecffee14751a160
jtmccutcheon <jt.mccutcheon@live.com>
2019-12-26 01:37:52 -0500

    sdfsd

--- src/components/login.js
+++ src/components/login.js
@@ -22,6 +22,19 @@ const Login = () => {
 				console.log(res, 'res dont you see halo 3');
 			})
 			.catch(err => console.log(err, 'for sure error'));
+		// fetch('https://newpkmtcg.herokuapp.com/api-token-auth/', user, {
+		// 	method: 'POST',
+		// 	body: JSON.stringify(user),
+		// 	mode: 'no-cors',
+		// 	headers: {
+		// 		'Content-Type': 'application/json',
+		// 		Accept: 'application/json',
+		// 	},
+		// })
+		// 	.then(res => {
+		// 		console.log(res, 'res dont you see halo 3');
+		// 	})
+		// 	.catch(err => console.log(err, 'for sure error'));
 	};
 
 	return (
@@ -34,6 +47,7 @@ const Login = () => {
 					type='text'
 					onChange={userInput}
 					value={user.username}
+					autoComplete='off'
 				/>
 				<br />
 				<label>password</label> <br />
@@ -43,6 +57,7 @@ const Login = () => {
 					type='password'
 					onChange={userInput}
 					value={user.password}
+					autoComplete='off'
 				/>
 				<br />
 				<button>log in</button>
